dfb99e9d8b6124d3bb6694282cadcafd6345196e
martinfpas <martinfpas@gmail.com>
2024-05-15 21:27:42 -0300

    form

--- src/components/NewProject.tsx
+++ src/components/NewProject.tsx
@@ -1,6 +1,15 @@
-import Stack from "@mui/material/Stack"
+import Stack from "@mui/material/Stack";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Project as ProjectType } from "../types/types";
+import Input from "@mui/material/Input";
+import { Button } from "@mui/material";
+
+
 
 export const NewProject = () => {
+    const { register, handleSubmit } = useForm<ProjectType>();
+    const onSubmit: SubmitHandler<ProjectType> = data => console.log(data);
+
     return <Stack alignItems={"center"} justifyContent={"center"} direction={"column"}>
         <Stack >
             <Stack direction={"row"} spacing={2}>
@@ -11,8 +20,26 @@ export const NewProject = () => {
             </Stack>
         </Stack>
 
-        <Stack width={"100%"} >
-            Form
+        <Stack width={"100%"} alignItems={"center"}>
+            <form onSubmit={handleSubmit(onSubmit)} >
+                <Stack spacing={2}>
+                    <Stack>
+                        <label>Name</label>
+                        <Input type="contained" {...register("name")} />
+                    </Stack>
+                    <Stack>
+                        <label>Description</label>
+                        <Input type="contained" {...register("description")} />
+                    </Stack>
+                    <Stack>
+                        <label>Owner</label>
+                        <Input type="contained" {...register("owner")} />
+                    </Stack>
+                    <Stack>
+                        <Button variant="contained" type="submit">Save</Button>
+                    </Stack>
+                </Stack>
+            </form>
         </Stack>
     </Stack>
 }
